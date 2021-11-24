12b02024835da72010972065cd76d69f7ddd34a6
tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>
2021-11-25 02:32:01 +0900

    feat: リソースの変更がある

--- src/tf/main.tf
+++ src/tf/main.tf
@@ -1,5 +1,5 @@
-# module "ec2" {
-#     source = "../modules/ec2"
-#     instance_type = "t2.micro"
-#     ami_id = "ami-0c07ed1dc0cea3608"
-# }
+module "ec2" {
+    source = "../modules/ec2"
+    instance_type = "t2.micro"
+    ami_id = "ami-0c07ed1dc0cea3608"
+}

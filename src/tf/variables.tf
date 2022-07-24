8ba149a321339d887609725de491205bc401e363
tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>
2022-07-25 01:53:27 +0900

    add: 実装追加

--- src/tf/variables.tf
+++ src/tf/variables.tf
@@ -5,3 +5,15 @@ variable "service_name" {
 variable "account_id" {
   default = "0000000000"
 }
+
+variable "awesome_domain" {
+  default = "awsometatsukoni.com"
+}
+
+data "aws_ssm_parameter" "awesome_certificate_arn" {
+  name = "/acm/awesome_certificate_arn"
+}
+
+data "aws_ssm_parameter" "awesome_route53_zone_id" {
+  name = "/route53/awesome_route53_zone_id"
+}
